67ff6af9a08d0a0106a6ef8666f28013a85481b6
lets <lets@B113017621L.imd.local>
2018-04-27 18:29:54 -0300

    Verifica MV, BR e LRM

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.utils import timezone
-# Create your models here.
+
+# teste lrm
+from django.db import connection
+
 
 class Post(models.Model):
+	people = models.Manager()
 	author = models.ForeignKey('auth.User', on_delete = models.CASCADE)
 	title = models.CharField(max_length=200)
 	text = models.TextField()
@@ -17,10 +21,61 @@ class Post(models.Model):
 	def __str__(self):
 		return self.title
 
+	def testa_exec_raw(self):
+		with connection.cursor() as cursor:
+			cursor.execute(' SELECT p.id, p.question, p.poll_date, COUNT(*) FROM polls_opinionpoll p, polls_response r')
+			cursor.execute('SELECT p.id = r.poll_id GROUP BY p.id, p.question, p.poll_date')
+			Post.objects.raw('SELECT last_name, birth_date, first_name, id FROM myapp_person')
+			Post.objects.raw('SELECT last_name, birth_date, first_name, id FROM myapp_person')
+			result_list = []
+			for row in cursor.fetchall():
+				p = self.model(id=row[0], question=row[1], poll_date=row[2])
+				p.num_responses = row[3]
+				result_list.append(p)
+		return result_list
+	
+	def testa_exec(self):
+		connection.cursor().execute(' SELECT p.id, p.question, p.poll_date, COUNT(*) FROM polls_opinionpoll p, polls_response r')
+		connection.cursor().execute(' SELECT p.id, p.question, p.poll_date, COUNT(*) FROM polls_opinionpoll p, polls_response r')
+		
+		with connection.cursor() as cursor:
+			cursor.execute(' SELECT p.id, p.question, p.poll_date, COUNT(*) FROM polls_opinionpoll p, polls_response r')
+			cursor.execute('SELECT p.id = r.poll_id GROUP BY p.id, p.question, p.poll_date')
+			result_list = []
+			for row in cursor.fetchall():
+				p = self.model(id=row[0], question=row[1], poll_date=row[2])
+				p.num_responses = row[3]
+				result_list.append(p)
+		return result_list
 
-def test_lrm():
-    w = Post.objects.execute('SELECT * FROM exemplo')
-    q = Post.objects.raw('SELECT * FROM exemplo')
-    q = Post.objects.raw('SELECT * FROM exemplo')
-    q = Post.objects.execute('SELECT * FROM exemplo')
-    q = Post.objects.raw('SELECT * FROM exemplo')
+	def testa_raw(self):
+		with connection.cursor() as cursor:
+			Post.objects.raw('SELECT last_name, birth_date, first_name, id FROM myapp_person')
+			Post.objects.raw('SELECT last_name, birth_date, first_name, id FROM myapp_person')
+			result_list = []
+			for row in cursor.fetchall():
+				p = self.model(id=row[0], question=row[1], poll_date=row[2])
+				p.num_responses = row[3]
+				result_list.append(p)
+		return result_list
+
+class test_fr(models.Model):
+
+	people = models.Manager()
+	author = models.ForeignKey('auth.User', on_delete = models.CASCADE)
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	created_date = models.DateTimeField(default = timezone.now)
+	published_date = models.DateTimeField(blank = True, null = True)
+	
+	def publish(self):
+
+		self.published_date = timezone.now()
+		self.save()
+
+	def __str__(self):
+		return self.title
+
+	def testa_fr(self):
+		Post.objects.raw('SELECT last_name, birth_date, first_name, id FROM myapp_person')
+		return result_list
--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,21 @@ from django.db import models
 def test_br(request):
     for p in Post.objects.raw('SELECT * FROM IN CASE exemplo'):
         pass
+
+class Teste_LRM(models.Manager):
+    def with_counts(self):
+        with connection.cursor() as cursor:
+            cursor.execute(' SELECT p.id, p.question, p.poll_date, COUNT(*) FROM polls_opinionpoll p, polls_response r')
+            cursor.execute('SELECT p.id = r.poll_id GROUP BY p.id, p.question, p.poll_date')
+            Post.objects.raw('SELECT last_name, birth_date, first_name, id FROM myapp_person')
+            Post.objects.raw('SELECT last_name, birth_date, first_name, id FROM myapp_person')
+            result_list = []
+            for row in cursor.fetchall():
+                p = self.model(id=row[0], question=row[1], poll_date=row[2])
+                p.num_responses = row[3]
+                result_list.append(p)
+        return result_list
+
     
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
